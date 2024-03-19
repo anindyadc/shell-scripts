2a453add821a1fe55886eeb9b5b19087699feaaf
anindyadc <anindyadc@gmail.com>
2024-03-20 02:34:40 +0530

    Added s3 support, backup folder, timestamp suffix
    Added s3 support, backup folder, timestamp suffix

--- site-tools/export_wordpress.sh
+++ site-tools/export_wordpress.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# prerequisite
-# sudo apt install pv
-
 # Define variables
+timestamp=$(date +"%Y%m%d_%H%M%S")
 wordpress_path="/var/www/html"
+backup_path="/home/ubuntu/backup"
 mysql_user="wpuser"
 mysql_password="password"
 mysql_db="wpdb"
+s3_bucket="mashmaristore"
+s3_prefix="backups/mashmari.in"
 
-# Export WordPress files
+# Export WordPress files with timestamp
 echo "Exporting WordPress files..."
-tar cf - -C $wordpress_path . | pv -s $(du -sb $wordpress_path | awk '{print $1}') | gzip > wordpress_files.tar.gz
+tar cf - -C $wordpress_path . | pv -s $(du -sb $wordpress_path | awk '{print $1}') | gzip > $backup_path/wordpress_files_$timestamp.tar.gz
 
-# Export MySQL database
+# Export MySQL database with timestamp
 echo "Exporting MySQL database..."
-mysqldump -u $mysql_user -p$mysql_password $mysql_db | pv | gzip > wordpress_database.sql.gz
+mysqldump -u $mysql_user -p$mysql_password $mysql_db | gzip > $backup_path/wordpress_database_$timestamp.sql.gz
+echo "Export complete. Files: wordpress_files_$timestamp.tar.gz, Database: wordpress_database_$timestamp.sql.gz"
 
-echo "Export complete. Files: wordpress_files.tar.gz, Database: wordpress_database.sql.gz"
+# Upload files to S3
+echo "Uploading files to S3..."
+aws s3 cp $backup_path/wordpress_files_$timestamp.tar.gz s3://$s3_bucket/$s3_prefix/
+aws s3 cp $backup_path/wordpress_database_$timestamp.sql.gz s3://$s3_bucket/$s3_prefix/
+
+echo "Export complete. Files uploaded to S3 bucket: $s3_bucket/$s3_prefix"
